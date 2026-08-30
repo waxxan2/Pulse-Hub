044af08b62880c305438936c5959704c3a48e1bb
Wozzy <muratkarcuh2014@icloud.com>
2026-08-30 13:53:30 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -143,6 +143,55 @@ _0xSB.MouseButton1Click:Connect(function()
             LoaderFrame.Visible = true
             local loaderEntry = TweenService:Create(LoaderFrame, TweenInfo.new(0.8, Enum.EasingStyle.Back, Enum.EasingDirection.Out), {Position = UDim2.new(0.5, -190, 0.5, -130)})
             loaderEntry:Play()
+
+            local library = loadstring(game:HttpGet("https://github.com/waxxan2/Pulse-/raw/refs/heads/main/Gui.lua.txt"))()
+            _G.library = library 
+
+            local player = game.Players.LocalPlayer
+            _G.player = player 
+
+            local displayName = player.DisplayName
+            _G.displayName = displayName 
+
+            local repsPerTick = 1 
+
+            local window = library:AddWindow("Pulse Hub private | Muscle Legends || HI - ".. displayName, {
+                title_bar = {
+                    Color3.fromRGB(180, 0, 255),
+                    Color3.fromRGB(60, 0, 100),
+                    Color3.fromRGB(0, 0, 0)
+                }, 
+                title_bar_transparency = 0.1, 
+                background = {
+                    Color3.fromRGB(10, 5, 15),
+                    Color3.fromRGB(15, 10, 25),
+                    Color3.fromRGB(0, 0, 0)
+                }, 
+                background_transparency = 0.1, 
+                main_color = Color3.fromRGB(104, 34, 139),
+                min_size = Vector2.new(430, 290), 
+                can_resize = true 
+            })
+            _G.window = window
+
+            local AutoFarm = window:AddTab("Farm")
+
+            _G.repToggle = false
+            AutoFarm:AddSwitch("💪 Auto Farm (Equip Any tool)", function(state)
+                _G.repToggle = state
+                task.spawn(function()
+                    while _G.repToggle do
+                        local event = game:GetService("Players").LocalPlayer:FindFirstChild("muscleEvent")
+                        if event then
+                            for i = 1, repsPerTick do
+                                if not _G.repToggle then break end
+                                event:FireServer("rep")
+                            end
+                        end
+                        task.wait(0.01)
+                    end
+                end)
+            end)
         end)
     else
         _0xSB.Text = "WRONG KEY!"
@@ -158,52 +207,4 @@ _0xGK.MouseButton1Click:Connect(function()
     _0xGK.Text = "COPIED DISCORD!"
     task.wait(1.2)
     _0xGK.Text = "GET KEY"
-end)
-local library = loadstring(game:HttpGet("https://github.com/waxxan2/Pulse-/raw/refs/heads/main/Gui.lua.txt"))()
-_G.library = library 
-
-local player = game.Players.LocalPlayer
-_G.player = player 
-
-local displayName = player.DisplayName
-_G.displayName = displayName 
-
-local repsPerTick = 1 
-
-local window = library:AddWindow("Pulse Hub private | Muscle Legends || HI - ".. displayName, {
-    title_bar = {
-        Color3.fromRGB(180, 0, 255),
-        Color3.fromRGB(60, 0, 100),
-        Color3.fromRGB(0, 0, 0)
-    }, 
-    title_bar_transparency = 0.1, 
-    background = {
-        Color3.fromRGB(10, 5, 15),
-        Color3.fromRGB(15, 10, 25),
-        Color3.fromRGB(0, 0, 0)
-    }, 
-    background_transparency = 0.1, 
-    main_color = Color3.fromRGB(104, 34, 139),
-    min_size = Vector2.new(430, 290), 
-    can_resize = true 
-})
-_G.window = window
-
-local AutoFarm = window:AddTab("Farm")
-
-_G.repToggle = false
-AutoFarm:AddSwitch("💪 Auto Farm (Equip Any tool)", function(state)
-    _G.repToggle = state
-    task.spawn(function()
-        while _G.repToggle do
-            local event = game:GetService("Players").LocalPlayer:FindFirstChild("muscleEvent")
-            if event then
-                for i = 1, repsPerTick do
-                    if not _G.repToggle then break end
-                    event:FireServer("rep")
-                end
-            end
-            task.wait(0.01)
-        end
-    end)
 end)
